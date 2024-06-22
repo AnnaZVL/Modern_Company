d604d5516f88f7c02bf6b3c02bfa8dbe8bb49bd8
Anna Zadiraka <annazadiraka@gmail.com>
2024-06-22 15:09:36 +0300

    change hand

--- js/script.js
+++ js/script.js
@@ -19,63 +19,16 @@ document.addEventListener("DOMContentLoaded", () => {
                 $nav.classList.remove('open');
             }
         })
-    })
-
+    })    
     
-    // Иконки контактов
-    const $btnSocial = document.getElementById('social'),
-    $icons = document.getElementById('icons');
-
-    $btnSocial.addEventListener('click', () => {
-        $btnSocial.classList.toggle('open');
-        $icons.classList.toggle('open');
-    })
-
-    $socialLinks.forEach(link => {
-        link.addEventListener('click', (event) => {
-            const target = event.target;
-
-            if (target.closest('.header__link')) {
-                $btnSocial.classList.remove('open');
-                $icons.classList.remove('open');
-            }
-        })
-    })
-
     // Закрытие по эск
     window.addEventListener('keydown', () => {
         $burger.classList.remove('open');
-        $nav.classList.remove('open');
-        $btnSocial.classList.remove('open');
-        $icons.classList.remove('open');
+        $nav.classList.remove('open');        
     })
  
-    const starContainer = document.querySelector('.star-container');
-
-    const createStar = () => {
-        const star = document.createElement('div');
-        star.classList.add('star');
-
-        const size = Math.random() * 3 + 1 + 'px';
-
-        star.style.width = size;
-        star.style.height = size;
-        star.style.left = Math.random() * 100 + 'vw';
-        star.style.top = Math.random() * 100 + 'vh';
-        
-        star.style.setProperty('--direction', (Math.random() - 0.5) * 2); // направление по горизонтали
-        
-        starContainer.appendChild(star);
-
-        setTimeout(() => {
-            star.remove();
-        }, 5000); 
-    };
-
-    setInterval(createStar, 200); // Создание новой звезды каждые 200 мс
-
-    // Видеоплеер
-    const videoPlayer = document.getElementById('videoPlayer');
+  // Видеоплеер
+  const videoPlayer = document.getElementById('videoPlayer');
 
   // Массив с URL видеофайлов
   const videos = [
